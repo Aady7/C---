261fd63390a86237841c917e2fe0574e3bf67075
Aady <aditya.psinghciv22@iitbhu.ac.in>
2023-02-22 02:01:47 +0530

    first commit

--- tut34.cpp
+++ tut34.cpp
@@ -1,4 +1,16 @@
-//try  and catch mechANISM
-try{
+//memiry leaks
+//memory leaks 
+#include<iostream>
+using namespace std;
 
-}catch(__EXCEPTIONS&)
+int main(){
+    int *p_number{new int{56}}; //points to some addresss lets call the address
+    //should delete and resret
+    int number{45};
+    p_number=&number;//noe p_number points to address2, but the address one is still not 
+    //relased, becuse we have lost the for address one.
+    return 0;
+}
+
+
+//
