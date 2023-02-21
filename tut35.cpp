b48fa1d111de5e14df0aa846d6e5aa4f60918a54
Aady <aditya.psinghciv22@iitbhu.ac.in>
2023-02-22 03:18:25 +0530

    second commit

--- tut35.cpp
+++ tut35.cpp
@@ -19,6 +19,22 @@ int main(){
             cout<<"value : "<<p_scores[i]<<" : "<<*(p_scores + i )<<endl;
         }
     }
+     delete[] p_salaries;
+     p_salaries= nullptr;
+     delete[] p_scores;
+     p_scores= nullptr;
+     delete[] p_students;
+     p_students= nullptr;
+// static array vs dynamic arrays
+int scores [10]{1,3,34,3,3,4,45,554,6};
+//this can work with the std size
+cout<<"scores size"<<size(scores)<<endl;
+//staic arrays allowwed the range based 
+for(auto s:scores){
+    cout<<"values :"<<s<<endl;
+}
 
+//dynamic array do not allow the size function and do not alloew the ranged based
+//for loop
     return 0;
 }
